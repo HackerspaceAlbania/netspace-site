8b005ccaeb3b54985b2d97e3618a723840ece777
Kilton Pisli <59768393+kiltonpisli@users.noreply.github.com>
2023-04-07 01:08:12 +0200

    Redesign Navtabs (#2)

--- src/components/Navtabs/index.tsx
+++ src/components/Navtabs/index.tsx
@@ -1,17 +1,24 @@
 import * as React from "react"
 import * as styles from './Navtabs.module.css'
 import { Link } from 'gatsby'
+import { useLocation } from '@reach/router';
 
 const Navtabs = () => {
+  const location = useLocation();
+
+  console.log(location)
+
   return (
-    <nav className={styles.navtabsComponent}>
-        <Link to="/">Root</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/events">Events</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/support">Support Us</Link>
-    </nav>
+    <div className={styles.navtabsComponent}>
+      <nav>
+        <Link to="/" className={location?.pathname === "/" ? styles.active : ""}>Root</Link>
+        <Link to="/projects" className={location?.pathname === "/projects/" ? styles.active : ""}>Projects</Link>
+        <Link to="/events" className={location?.pathname === "/events/" ? styles.active : ""}>Events</Link>
+        <Link to="/about" className={location?.pathname === "/about/" ? styles.active : ""}>About</Link>
+        <Link to="/contact" className={location?.pathname === "/contact/" ? styles.active : ""}>Contact</Link>
+        <Link to="/support" className={location?.pathname === "/support/" ? styles.active : ""}>Support Us</Link>
+      </nav>
+    </div>
   )
 }
 
--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -11,8 +11,12 @@ const Page: FC<PageProps> = (props) => {
   return (
     <div className={styles.pageComponent}>
         <Navtabs />
-        <h1>{props.title}</h1>
-        {props.children}
+        <div className={styles.pageContent}>
+          <br />
+          <h1>{props.title}</h1>
+          {props.children}
+          <br />
+        </div>
     </div>
   )
 }
